4d8cfe08e32fc45c77f49204fa1f6d22a623a77c
Brandon Tran <brandon.tran.khang@gmail.com>
2021-11-15 16:14:51 -0700

    Storage
    Created a new storage class

---
+++ src/JavaCode/Storage.java
@@ -0,0 +1,14 @@
+package JavaCode;
+
+import java.util.ArrayList;
+
+public class Storage {
+
+	private static ArrayList<Patient> allPatients;
+	private static ArrayList<Doctor> allDoctors;
+	private static ArrayList<Nurse> allNurses;
+	
+	Storage(){
+		
+	}
+}
