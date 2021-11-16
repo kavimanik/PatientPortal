255dd6ac56d08adaa1f18f4d3c22056034f7847b
Brandon Tran <brandon.tran.khang@gmail.com>
2021-11-15 23:37:07 -0700

    Added Size Function
    Get a size of of any of the array list

--- src/JavaCode/Storage.java
+++ src/JavaCode/Storage.java
@@ -12,12 +12,23 @@ public class Storage {
 	
 	Storage(){}
 	
+	// Getter, returns the variable from ArrayList
 	// i is the index of the variable in its respective Array List
 	static Nurse getNurse(int i) { return allNurses.get(i); }
 	static Doctor getDoctor(int i) { return allDoctors.get(i); }
 	static Patient getPatient(int i) { return allPatients.get(i); }
 	
-	static void setNurse(Nurse i) { allNurses.add(i); }
-	static void setDoctor(Doctor i) { allDoctors.add(i); }
-	static void setPatient(Patient i) { allPatients.add(i); }
+	// Gets the Size of any of the ArrayLists with "Doctor", "Nurse" or "Patient"
+	static int getSize(String a) {
+		switch(a) {
+		case "Doctor": return allDoctors.size();
+		case "Nurse": return allNurses.size();
+		case "Patient": return allPatients.size();
+		default: return -1; // Should never reach here
+	}}
+	
+	// Adder, adds a class to the respective linkedList
+	static void addNurse(Nurse i) { allNurses.add(i); }
+	static void addDoctor(Doctor i) { allDoctors.add(i); }
+	static void addPatient(Patient i) { allPatients.add(i); }
 }
