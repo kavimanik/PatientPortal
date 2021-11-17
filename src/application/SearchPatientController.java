7ad3d6e8b3a8684c84a01626e3744c0847d12b0d
Kavi <kavimanik89@gmail.com>
2021-11-17 00:06:25 -0700

    Changed SearchPatient

--- src/application/SearchPatientController.java
+++ src/application/SearchPatientController.java
@@ -1,7 +1,16 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 import JavaCode.*;
 
 public class SearchPatientController {
@@ -9,4 +18,19 @@ public class SearchPatientController {
 	@FXML // fx:id="lvSearches"
 	private ListView lvSearches;
 	
+	@FXML // fx:id="tfLastName"
+	private TextField tfLastName;
+	
+	@FXML // fx:id="tfFirstName"
+	private TextField tfFirstName;
+	
+	@FXML
+	private void populateListView(ActionEvent event) throws IOException {
+		event.consume();
+		lvSearches.getItems().clear();
+		String firstName = tfFirstName.getText();
+		String lastName = tfLastName.getText();
+	    
+	    
+	}
 }
