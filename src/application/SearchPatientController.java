f01084bbf84eec03e166c64ac55656ad97084c29
Kavi <kavimanik89@gmail.com>
2021-11-16 23:45:10 -0700

    Created controllers

---
+++ src/application/SearchPatientController.java
@@ -0,0 +1,12 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import JavaCode.*;
+
+public class SearchPatientController {
+	
+	@FXML // fx:id="lvSearches"
+	private ListView lvSearches;
+	
+}
