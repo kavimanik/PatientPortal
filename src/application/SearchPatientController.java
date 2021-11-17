4354c2ea02b8af3282cd535163ff046d2db3f8ce
Kavi <kavimanik89@gmail.com>
2021-11-17 16:24:14 -0700

    added functionality to searchPatient

--- src/application/SearchPatientController.java
+++ src/application/SearchPatientController.java
@@ -1,5 +1,6 @@
 package application;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,7 +8,10 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -35,5 +39,24 @@ public class SearchPatientController {
 		for(int i = 0; i < list.size(); i++) {
 			lvSearches.getItems().add(list.get(i).getFirstName() + " " + list.get(i).getLastName() + " " + list.get(i).getBirth());
 		}
+	}
+	
+	@FXML
+	private void handleMouseClick(MouseEvent event) throws IOException {
+		event.consume();
+		String patient = lvSearches.getSelectionModel().getSelectedItem().toString();
+		Node node = (Node) event.getSource();
+        Stage thisStage = (Stage) node.getScene().getWindow();
+        AnchorPane loader = FXMLLoader.load(getClass().getResource("../PatientMedicalInfo.fxml"));
+        thisStage.getScene().setRoot(loader);
+        ObservableList<Node> list = loader.getChildren();
+        for(int i = 0; i < list.size(); i++) {
+        	if(list.get(i).getId() != null && list.get(i).getId().equals("tfPatientName")) {
+        		System.out.println(patient);
+        		TextField text = (TextField) list.get(i);
+        		text.setText(patient);
+        		return;
+        	}
+        }
 	}
 }
