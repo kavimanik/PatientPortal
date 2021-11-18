8eb9ee01f0b3a6d1a892ca9e8bf30766d0f91c4c
Kavi <kavimanik89@gmail.com>
2021-11-17 20:56:17 -0700

    finished patient profile

--- src/application/PatientPageController.java
+++ src/application/PatientPageController.java
@@ -2,11 +2,15 @@ package application;
 
 import java.io.IOException;
 
+import JavaCode.*;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class PatientPageController {
@@ -15,11 +19,46 @@ public class PatientPageController {
 	@FXML
     private void switchToPatientProfile(ActionEvent event) throws IOException {
         event.consume();
+        
+        User user = Storage.getCurrentUser();
+        String firstName = user.getFirstName();
+        String lastName = user.getLastName();
+        Patient patient = Storage.searchPatient(firstName, lastName);
+        
         Node node = (Node) event.getSource();
         Stage thisStage = (Stage) node.getScene().getWindow();
-        //thisStage.hide();
-        Parent loader = FXMLLoader.load(getClass().getResource("../PatientProfile.fxml"));
+        AnchorPane loader = FXMLLoader.load(getClass().getResource("../PatientProfile.fxml"));
         thisStage.getScene().setRoot(loader);
+        
+        
+        ObservableList<Node> list = loader.getChildren();
+        for(int i = 0; i < list.size(); i++) {
+        	if(list.get(i).getId() != null && list.get(i).getId().equals("ftName")) {
+        		TextField ftName = (TextField) list.get(i);
+        		ftName.setText(firstName + " " + lastName);
+        	}
+        	else if(list.get(i).getId() != null && list.get(i).getId().equals("tfBirthday")) {
+        		TextField tfBirthday = (TextField) list.get(i);
+        		tfBirthday.setText(patient.getBirth());
+        	}
+        	else if(list.get(i).getId() != null && list.get(i).getId().equals("ftPhoneNumber")) {
+        		TextField ftPhoneNumber = (TextField) list.get(i);
+        		ftPhoneNumber.setText(patient.getPhoneNumber());
+        	}
+        	else if(list.get(i).getId() != null && list.get(i).getId().equals("ftEmail")) {
+        		TextField ftEmail = (TextField) list.get(i);
+        		ftEmail.setText(patient.getEmail());
+        	}
+        	else if(list.get(i).getId() != null && list.get(i).getId().equals("ftInsurance")) {
+        		TextField ftInsurance = (TextField) list.get(i);
+        		ftInsurance.setText(patient.getInsurance());
+        	}
+        	else if(list.get(i).getId() != null && list.get(i).getId().equals("ftPharmacy")) {
+        		TextField ftPharmacy = (TextField) list.get(i);
+        		ftPharmacy.setText(patient.getPharmacy());
+        	}
+        	
+        }
     }
 	
 	@FXML
